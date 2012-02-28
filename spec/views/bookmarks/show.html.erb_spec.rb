878e7a8b18b8a5a2c66b1e7f3342455d0e87caeb
Ralph Bopp <ralphbopp@gmail.com>
2012-02-28 17:15:30 -0600

    created scaffold

---
+++ spec/views/bookmarks/show.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "bookmarks/show.html.erb" do
+  before(:each) do
+    @bookmark = assign(:bookmark, stub_model(Bookmark,
+      :url => "Url",
+      :name => "Name",
+      :date => "Date"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Url/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Name/)
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Date/)
+  end
+end
